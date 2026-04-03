d5ce9c9ea6a61dc894245af417bbf5454b341e0d
Rexshan <rexshansam22@gmail.com>
2026-04-03 23:09:32 +0530

    Update lockerAPI endpoints to include '/api' prefix

--- frontend/src/api/lockerAPI.js
+++ frontend/src/api/lockerAPI.js
@@ -2,11 +2,10 @@ import axiosInstance from './axiosInstance'
 
 export const lockerAPI = {
   getAll: (params = {}) => {
-    return axiosInstance.get('/lockers/', { params })
+    return axiosInstance.get('/api/lockers/', { params })
   },
-
   getById: (id) => {
-    return axiosInstance.get(`/lockers/${id}/`)
+    return axiosInstance.get(`/api/lockers/${id}/`)
   },
 
   create: (lockerData) => {
