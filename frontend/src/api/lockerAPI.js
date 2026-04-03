bc912c87fa04e7205e5270865c66b5e4e706963d
Rexshan <rexshansam22@gmail.com>
2026-04-03 23:31:15 +0530

    Remove 'api' prefix from locker API endpoints

--- frontend/src/api/lockerAPI.js
+++ frontend/src/api/lockerAPI.js
@@ -2,21 +2,21 @@ import axiosInstance from './axiosInstance'
 
 export const lockerAPI = {
   getAll: (params = {}) => {
-    return axiosInstance.get('/api/lockers/', { params })
+    return axiosInstance.get('/lockers/', { params })
   },
   getById: (id) => {
-    return axiosInstance.get(`/api/lockers/${id}/`)
+    return axiosInstance.get(`/lockers/${id}/`)
   },
 
   create: (lockerData) => {
-    return axiosInstance.post('api/lockers/', lockerData)
+    return axiosInstance.post('/lockers/', lockerData)
   },
 
   update: (id, lockerData) => {
-    return axiosInstance.put(`api/lockers/${id}/`, lockerData)
+    return axiosInstance.put(`/lockers/${id}/`, lockerData)
   },
 
   delete: (id) => {
-    return axiosInstance.delete(`api/lockers/${id}/`)
+    return axiosInstance.delete(`/lockers/${id}/`)
   },
 }
